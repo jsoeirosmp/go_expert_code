06aa03fa49f5c98a98f31c5e18981dfa460bcc46
Joao Soeiro <joao.soeiro@sumup.com>
2024-02-27 19:21:42 -0300

    Added comments for aula 13 and created aula 14 file

--- aula_14_interfaces/interfaces.go
+++ aula_14_interfaces/interfaces.go
@@ -12,15 +12,17 @@ type Endereco struct {
 // A interface é um conjunto de métodos com o qual as structs podem se conformar, neste caso, todo cliente
 // pode ser ativada ou desativada, entao adiciono ele a essa interface Pessoa, que contem metodos comuns a
 // todos que eu considero "pessoas". Lembrar exemplo interface animal Sound(), struct dog, dog entra na interface
-// animal pois todo animal emite som
+// animal pois todo animal emite som, interfaces aceitam SOMENTE METODOS, e nao outras propriedades como "nome:"
 type Pessoa interface {
 	Desativar()
 }
 
+// Criando segunda struct
 type Empresa struct {
 	Nome string
 }
 
+// Func somente com metodo desativar
 func (e Empresa) Desativar() {
 
 }
@@ -59,4 +61,10 @@ func main() {
 	// Pq posso passar joao se o type recebido dessa func é uma pessoa? Pq a var joao é considerada uma pessoa
 	// por implementar esse metodo desativar
 	Desativacao(joao)
+
+	// Declaro a var de empresa vazia so pra teste, e ao usar o metodo Desativacao(), funciona do mesmo jeito
+	// porque fiz aquela func recebendo empresa que tem o metodo Desativar, ou seja, TODA struct com o metodo
+	// desativar sera considerada uma "pessoa" pois vai implementar a interface
+	minhaEmpresa := Empresa{}
+	Desativacao(minhaEmpresa)
 }
